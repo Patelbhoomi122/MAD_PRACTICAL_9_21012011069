b562abf10b723f1621ae7822f2915bc443696b13
hp <patelbhoomi3203@gmail.com>
2023-10-03 10:37:30 +0530

    practical_9

---
+++ app/src/main/java/com/example/mad_practical_9_21012011069/Splash_Activity.kt
@@ -0,0 +1,11 @@
+package com.example.mad_practical_9_21012011069
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Splash_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
